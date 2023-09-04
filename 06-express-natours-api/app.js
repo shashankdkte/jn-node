33065392d0a68ead2c2cde3737764fc9357815ed
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-04 16:17:16 +0530

    Handing POST Request

--- 06-express-natours-api/app.js
+++ 06-express-natours-api/app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const PORT = 3000;
 
 const app = express();
+
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -15,7 +18,21 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: newTour,
+      });
+    }
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
