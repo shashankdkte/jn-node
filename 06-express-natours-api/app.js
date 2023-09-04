5a99b90c2c9fe9f4e012ebeb81522be7fc7036d6
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-04 16:06:35 +0530

    Handling GET Requests

--- 06-express-natours-api/app.js
+++ 06-express-natours-api/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 const PORT = 3000;
 
 const app = express();
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.get('/', (req, res) => {
-  res.status(200).send('This is home');
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: tours,
+  });
 });
 
-app.get('/tours', (req, res) => {
-  res.status(200).send('This is tours');
-});
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
