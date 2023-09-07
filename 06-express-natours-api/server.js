24b92aa79038ef38cd469934c739b78141807d53
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-07 23:48:21 +0530

    Connecting our Database with the Express App

--- 06-express-natours-api/server.js
+++ 06-express-natours-api/server.js
@@ -1,8 +1,23 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connection successful!"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
