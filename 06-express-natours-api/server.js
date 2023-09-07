acd458e57a7d719b374ad57da3051d5009a81a54
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-07 23:59:42 +0530

    Creating Documents and Testing Model

--- 06-express-natours-api/server.js
+++ 06-express-natours-api/server.js
@@ -37,6 +37,21 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: "The Firest Hiker",
+  rating: 4.7,
+  price: 497
+});
+
+testTour.save()
+  .then((doc) => {
+  console.log(doc)
+  }).catch(err => {
+    console.log(err)
+  }
+  )
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
