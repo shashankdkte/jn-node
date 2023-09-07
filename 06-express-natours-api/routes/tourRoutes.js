49a5da448405c0862f66fcc3320dceb8fa38838d
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-08 00:26:26 +0530

    Refactoring for MVC and Creating Document

--- 06-express-natours-api/routes/tourRoutes.js
+++ 06-express-natours-api/routes/tourRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const tourController = require("../controllers/toursController")
 
-router.param("id", tourController.checkID);
+// router.param("id", tourController.checkID);
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody,tourController.createTour);
+  .post(tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
