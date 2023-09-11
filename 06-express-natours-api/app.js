44a1caf7666e7591652f61a5b36d45fdc22658ca
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-11 20:02:03 +0530

    Handling Unhandled Routes

--- 06-express-natours-api/app.js
+++ 06-express-natours-api/app.js
@@ -31,5 +31,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
+/* HANDLING UNHANDLED ROUTES*/
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Cant find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
