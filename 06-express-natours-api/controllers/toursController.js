73b4b8628f3223824637a0fede4e3a48e4a564f1
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-11 20:46:21 +0530

    Adding 404 Not Found Errors

--- 06-express-natours-api/controllers/toursController.js
+++ 06-express-natours-api/controllers/toursController.js
@@ -1,5 +1,6 @@
 const Tour = require("../models/tourModel");
 const APIFeatures = require("../utils/apiFeatures");
+const AppError = require("./../utils/appError");
 const catchAsync = require("./../utils/catchAsync")
 
 exports.aliasTopTours = (req, res, next) => {
@@ -35,7 +36,9 @@ exports.getTour = catchAsync(async (req, res,next) => {
  
     const tour = await Tour.findById(req.params.id);
     // Tour.findOne({ _id: req.params.id })
-
+     if (!tour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
     res.status(200).json({
       status: "success",
       data: {
@@ -51,7 +54,9 @@ exports.updateTour = catchAsync(async(req, res,next) => {
     new: true,
     runValidators:true
   })
-
+ if (!tour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
   res.status(200).json({
     status: "success",
     data: {
@@ -62,7 +67,10 @@ exports.updateTour = catchAsync(async(req, res,next) => {
 
 exports.deleteTour = catchAsync(async (req, res,next )=> {
   
-    await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+   if (!tour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
 
     res.status(204).json({
       status: "success",
