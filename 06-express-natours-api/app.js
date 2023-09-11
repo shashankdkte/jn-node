c1dae50c6cb232971dba99c453a8c23fe7bdec61
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-11 20:10:15 +0530

    Implementing a Global Error Handling Middleware

--- 06-express-natours-api/app.js
+++ 06-express-natours-api/app.js
@@ -31,12 +31,24 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
-/* HANDLING UNHANDLED ROUTES*/
+/** HANDLING UNHANDLED ROUTES*/
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Cant find ${req.originalUrl} on this server`,
-  });
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Cant find ${req.originalUrl} on this server`,
+  // });
+  const er = new Error(`Cant find ${req.originalUrl} on the server`);
+  er.status = "fail";
+  er.status = 404;
+next(er)
 });
 
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 module.exports = app;
