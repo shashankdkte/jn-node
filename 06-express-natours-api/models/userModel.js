a15a5caeb6eadb0ec0767b803c328c1fc5da956b
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-12 09:34:32 +0530

    Managing Password

--- 06-express-natours-api/models/userModel.js
+++ 06-express-natours-api/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,10 +23,30 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password']
+    required: [true, 'Please confirm your password'],
+    validate: {
+      //This only works on create and save
+      validator: function (el) {
+        return el == this.password
+      },
+
+      message:"Password are not the same"
+    }
   }
 });
 
+userSchema.pre("save", async function (next) {
+  //only run this function if password was not actually modified
+  if (!this.isModified('password')) return next();
+
+  //Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete the passwordConfirm field
+
+  this.passwordConfirm = undefined;
+  next();
+})
 const User = mongoose.model('User', userSchema);
 
 module.exports = User
