dc279b1e0ae20616c9308e2fc0b53406b129f303
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-15 14:55:50 +0530

    Authoriaztion User Roles and permission

--- 06-express-natours-api/controllers/authController.js
+++ 06-express-natours-api/controllers/authController.js
@@ -89,3 +89,14 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 })
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    
+    if (!roles.includes(req.user.role))
+    {
+      return next(new AppError("You do not have the permission to perform this action"));
+    }
+    next();
+  }
+}
