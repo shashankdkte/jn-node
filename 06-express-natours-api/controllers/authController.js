92d33d249c44b92404db434fbae72cda56a3864a
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-15 18:10:22 +0530

    Update my password

--- 06-express-natours-api/controllers/authController.js
+++ 06-express-natours-api/controllers/authController.js
@@ -5,11 +5,26 @@ const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const sendEmail = require("./../utils/email");
 const jwt = require("jsonwebtoken");
+const { create } = require("domain");
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn:process.env.JWT_EXPIRES_IN
   })
 }
+
+exports.createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  // Remove password from output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
+};
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
   // const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
@@ -174,4 +189,25 @@ exports.resetPassword = catchAsync ( async(req, res, next) => {
       user
     }
   })
+})
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  
+  // 1) Get user form collection
+
+  const user = await User.findById(req.user._id).select('+password');
+
+  // 2) Check if posted current password is correct
+  if (! await user.correctPassword(req.body.passwordCurrent, user.password))
+  {
+    return next(new AppError("Your current password is wrong", 401));
+  }
+  
+  // 3) If so update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+
+  await user.save();
+
+  this.createSendToken(user, 200, res);
 })
