0a536cec23444ece0e82cd68b564f51e3464504c
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-15 19:17:49 +0530

    Sending JWT via Cookie

--- 06-express-natours-api/controllers/authController.js
+++ 06-express-natours-api/controllers/authController.js
@@ -15,6 +15,15 @@ const signToken = (id) => {
 exports.createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   // Remove password from output
+
+  const cookieOptions = {
+    expiresIn: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly:true
+  }
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
   user.password = undefined;
 
   res.status(statusCode).json({
@@ -31,13 +40,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   //   expiresIn:process.env.JWT_EXPIRES_IN
   // })
   const token = signToken(newUser._id);
-  res.status(201).json({
-    status: "success",
-    token,
-    data: {
-      user:newUser
-    }
-  })
+ this.createSendToken(newUser, 201, res);
 })
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -59,13 +62,8 @@ exports.login = catchAsync(async (req, res, next) => {
 
   }
   
-  // 3) Return token
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: "success",
-    token,
-   
-  })
+ // 3) If everything ok, send token to client
+  this.createSendToken(user, 200, res);
 })
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -182,13 +180,8 @@ exports.resetPassword = catchAsync ( async(req, res, next) => {
   // 3) Update changedPasswordAt property for the user
   // 4) Log the user in, send JWT
   const token = signToken(user._id);
- res.status(200).json({
-    status: 'success',
-    token,
-    data: {
-      user
-    }
-  })
+ // 4) Log the user in, send JWT
+  this.createSendToken(user, 200, res);
 })
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
