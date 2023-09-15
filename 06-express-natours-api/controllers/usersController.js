c0f14527fd8cf9b2f59ad85b81746869a3ec9593
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-15 18:38:26 +0530

    Updating the current User Data

--- 06-express-natours-api/controllers/usersController.js
+++ 06-express-natours-api/controllers/usersController.js
@@ -1,3 +1,15 @@
+const User = require("../models/userModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+
+const filterObj = (obj, ...allowedFields) =>
+  {
+  const newObj = {}
+  Object.keys(obj).forEach(el => {
+    if(allowedFields.includes(el)) newObj[el] = obj[el]
+  })
+  return newObj;
+  }
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
     status: 'fail',
@@ -32,3 +44,30 @@ exports.deleteUser = (req, res) => {
     data: 'Not available',
   });
 };
+
+exports.updateMe = catchAsync( async(req, res) => {
+  
+  // 1) Create error if user posts password
+  if (req.body.password || req.body.passwordConfirm)
+  {
+    return next(new AppError("This route is not for updating passwords Please use /updateMyPassword"));
+  }
+  
+  // 2) Filtered out unwanted field names that are not allowed to be updated
+
+  const filterBody = filterObj(req.body, 'name', 'email');
+
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filterBody, {
+    new: true,
+    runValidators:true
+  })
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user:updatedUser
+    }
+  })
+})
